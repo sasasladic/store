2193523825685539c2ed6a4ad63bb59d566a5252
Uroš Krupniković <uros.krupnikovic@quantox.com>
2022-01-10 18:34:46 +0100

    Added sort for Profile order history

--- src/components/Profile/ProfileData.js
+++ src/components/Profile/ProfileData.js
@@ -1,11 +1,17 @@
 import axios from "axios";
 import { useEffect } from "react";
-import { Table, TableHead, TableBody, TableRow, TableCell, CircularProgress, TableContainer } from "@mui/material";
+import { Table, TableHead, TableBody, TableRow, TableCell, CircularProgress, TableContainer, Select, MenuItem, FormControl, InputLabel } from "@mui/material";
 import { useState } from "react";
 
 const ProfileData = () => {
 
   const [dom, setDom] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [sort, setSort] = useState('last_updated')
+
+  const sortHandler = (e) => {
+    setSort(e.target.value);
+  }
 
   useEffect(() => {
     const head = {
@@ -13,7 +19,9 @@ const ProfileData = () => {
         Authorization: `Bearer ${localStorage.getItem('token')}`
       }
     }
-    axios.get('https://api.orders.galeja.net/api/order', head).then(res => {
+    const url = sort ? `https://api.orders.galeja.net/api/order?sort=${sort}`  : 'https://api.orders.galeja.net/api/order';
+    setLoading(true);
+    axios.get(url, head).then(res => {
       const data = res.data.data;
       const domTemp = [];
       data.forEach((item, i) => {
@@ -21,29 +29,39 @@ const ProfileData = () => {
         domTemp.push(<TableRow key={i}>
           <TableCell>{item.sum}€</TableCell>
           <TableCell>{item.quantity}</TableCell>
-          <TableCell>{item.ordered_at}</TableCell>
+          <TableCell>{item.ordered_at.substring(0, 10)}</TableCell>
           <TableCell>{item.address}</TableCell>
           <TableCell style={{ color: statusColor }}>{item.status}</TableCell>
         </TableRow>);
       })
       setDom(domTemp);
-
+      setLoading(false)
     }).catch(err => {
+      setLoading(false)
       console.log(err);
     });
-  }, [])
+  }, [sort])
 
   return <div className="profileData">
     <div className="profileDataInner">
       <h3 style={{ opacity: 0.6, fontWeight: 'normal', marginLeft: 20 }}>Profile order history</h3>
-      {dom.length ? 
+      <FormControl>
+        <InputLabel id='label123'>Sort</InputLabel>
+        <Select value={sort} onChange={sortHandler} labelId="123" label='Sort'>
+          <MenuItem value='last_updated'>Last updated (newest)</MenuItem>
+          <MenuItem value='-last_updated'>Last updated (oldest)</MenuItem>
+          <MenuItem value='-sum'>Price (high)</MenuItem>
+          <MenuItem value='sum'>Price (low)</MenuItem>
+        </Select>
+      </FormControl>
+      {!loading ? 
       <TableContainer style={{ overflowX: 'auto' }}>
         <Table>
           <TableHead>
             <TableRow>
               <TableCell style={{fontWeight: 'bold'}}>Price</TableCell>
               <TableCell style={{fontWeight: 'bold'}}>Items</TableCell>
-              <TableCell style={{fontWeight: 'bold'}}>Date</TableCell>
+              <TableCell style={{fontWeight: 'bold'}}>Order Date</TableCell>
               <TableCell style={{fontWeight: 'bold'}}>Address</TableCell>
               <TableCell style={{fontWeight: 'bold'}}>Status</TableCell>
             </TableRow>
@@ -53,7 +71,7 @@ const ProfileData = () => {
           </TableBody>
         </Table>
         </TableContainer>  
-        : <div style={{display: 'flex', justifyContent: 'center'}}><CircularProgress></CircularProgress></div>}
+        : <div style={{display: 'flex', justifyContent: 'center', margin: '20px 0'}}><CircularProgress></CircularProgress></div>}
     </div>
   </div>
 }
